03fdca33b1464b003eed858bdf62e6054e5476af
Kaoru Monda <>
2024-11-11 20:12:25 +0900

    コンソールログの削除

--- src/utils/dateUtils.js
+++ src/utils/dateUtils.js
@@ -9,17 +9,16 @@ export const GetNow = () => {
   // Date オブジェクトに変換
   const dateWithMicroseconds = new Date(highPrecisionTime);
 
-  // マイクロ秒の部分を手動で設定
+  // ミリ秒までの部分を取得
   const isoString = dateWithMicroseconds.toISOString();
 
   // マイクロ秒部分を取得
   const microseconds = Math.floor((highPrecisionTime % 1000) * 1000) % 1000;
 
-  // マイクロ秒精度の時刻文字列を作成し、返す
+  // ミリ秒までの時刻にマイクロ秒を連結した文字列を返す
   const result = isoString.replace(
     "Z",
     `${String(microseconds).padStart(3, "0")}Z`
   );
-  console.log(result);
   return result;
 };
